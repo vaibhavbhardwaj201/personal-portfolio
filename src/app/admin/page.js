a93e63788b12f2245a537232025c1c5c050d188f
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-10-09 01:47:42 +0300

    Initial setup and Admin View

---
+++ src/app/admin/page.js
@@ -0,0 +1,39 @@
+"use client"
+
+import React from 'react'
+
+const AdminView = () => {
+
+    const menuItem = [
+        {
+            id: 'home',
+            label: 'Home'
+        },
+        {
+            id: 'about',
+            label: 'About'
+        },
+        {
+            id: 'experience',
+            label: 'Experience'
+        },
+        {
+            id: 'education',
+            label: 'Education'
+        },
+        {
+            id: 'projects',
+            label: 'Projects'
+        },
+        {
+            id: 'contact',
+            label: 'Contact'
+        },
+    ]
+
+  return (
+    <div>AdminView</div>
+  )
+}
+
+export default AdminView
---
+++ src/database/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log("DB connected");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default connectToDB
